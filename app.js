780c40687aad8e3b14280fd4f6cb874d1be28c46
Ccrazydreamer <bcrazydreamer@gmail.com>
2018-05-31 23:30:29 +0530

    a

--- app.js
+++ app.js
@@ -46,8 +46,8 @@ io.on('connection', function(socket){
   	var userip=socket.request.connection._peername.address;
   	dmsg.message=msg;
   	dmsg.user=userip.substring(7);
-  	dmsg.ucolor=dmsg[userip.substring(7)][0];
-    dmsg.uname=dmsg[userip.substring(7)][1];
+  	dmsg.ucolor='#E040FB';//dmsg[userip.substring(7)][0];
+    dmsg.uname="Anonymous";//dmsg[userip.substring(7)][1];
     io.emit('dchat msg', dmsg);
   });
 });
