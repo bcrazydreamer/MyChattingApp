f03535fa8c4647eed98c12442bde38a9e730daec
Ccrazydreamer <bcrazydreamer@gmail.com>
2018-06-02 11:04:39 +0530

    test

--- app.js
+++ app.js
@@ -19,21 +19,8 @@ app.use(function(req, res, next) {
 });
 
 
-app.get('/', function(req, res){
-  /*
-  //var ip=requestIp.getClientIp(req);
-var ip = req.headers["x-forwarded-for"];
-  if (ip){
-    var list = ip.split(",");
-    ip = list[list.length-1];
-  } else {
-    ip = req.connection.remoteAddress;
-  }
-
-    var userinfo=[colors[Math.floor(Math.random() * 14)]];
-    dmsg[ip.substring(7)]=userinfo;
-    dmsg["debug"]=ip; //for debugging only
-  */
+app.get('/', function(req, res)
+{
   res.sendFile(__dirname + '/index.html');
 });
 
@@ -43,29 +30,26 @@ app.get('/test', function(req, res){
 
 app.post("/",function(req,res)
 {
-  //var ip=requestIp.getClientIp(req);
-  //var username=req.body.user.name;
-  //dmsg[ip.substring(7)][1]=username;
   res.sendFile(__dirname + '/chatroom.html');
 });
-/*
-app.get('/chatroom', function(req, res){
-  res.status(300).sendFile(path.join(__dirname,"chatroom.html"));
-});*/
+
 
 app.get('/about', function(req, res){
   res.status(300).sendFile(path.join(__dirname,"about.html"));
 });
 
+app.get('/chatroom', function(req, res){
+  res.status(300).sendFile(path.join(__dirname,"chatroom.html"));
+});
 
 io.on('connection', function(socket){
   socket.on('dchat msg', function(msg){
       var userip=socket.request.connection.remoteAddress;
       console.log(userip);
-      dmsg.message=msg;//dmsg.debug+userip
+      dmsg.message=msg;
       dmsg.user=userip;
-      dmsg.ucolor=dmsg[userip][0];//[colors[Math.floor(Math.random() * 14)]];//
-      dmsg.uname=dmsg[userip][1];//"Anonymous";
+      dmsg.ucolor=dmsg[userip][0];
+      dmsg.uname=dmsg[userip][1];
       io.emit('dchat msg', dmsg);
     });
 
@@ -73,14 +57,9 @@ io.on('connection', function(socket){
     var ip=socket.request.connection.remoteAddress;
     var userinfo=[colors[Math.floor(Math.random() * 14)]];
     dmsg[ip]=userinfo;
-    dmsg.status=true;
     console.log(ip);
-
     var username=msg;
     dmsg[ip][1]=username;
-
-   // var destination = '/chatroom';
-  //  io.emit('redirect', destination);
     });
 });
 
