5e7b52b6fa938d79570b83b8075ceb34cf38d1af
Ccrazydreamer <bcrazydreamer@gmail.com>
2018-06-02 10:05:58 +0530

    a

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express=require("express");
 var app=express();
-var http = require('http').Server(app);
+var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 var path=require('path');
@@ -20,6 +20,7 @@ app.use(function(req, res, next) {
 
 
 app.get('/', function(req, res){
+  /*
   //var ip=requestIp.getClientIp(req);
 var ip = req.headers["x-forwarded-for"];
   if (ip){
@@ -32,8 +33,12 @@ var ip = req.headers["x-forwarded-for"];
     var userinfo=[colors[Math.floor(Math.random() * 14)]];
     dmsg[ip.substring(7)]=userinfo;
     dmsg["debug"]=ip; //for debugging only
-
+  */
   res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/test', function(req, res){
+  res.sendFile(__dirname + '/test.html');
 });
 
 app.post("/",function(req,res)
@@ -44,19 +49,38 @@ app.post("/",function(req,res)
   res.sendFile(__dirname + '/chatroom.html');
 });
 
+app.get('/chatroom', function(req, res){
+  res.status(300).sendFile(path.join(__dirname,"chatroom.html"));
+});
+
 app.get('/about', function(req, res){
   res.status(300).sendFile(path.join(__dirname,"about.html"));
 });
 
+
 io.on('connection', function(socket){
   socket.on('dchat msg', function(msg){
-  	var userip=socket.request.connection.remoteAddress;
-  	dmsg.message=dmsg.debug+userip;//msg;
-  	dmsg.user=userip.substring(7);
-  	dmsg.ucolor=[colors[Math.floor(Math.random() * 14)]];//'#E040FB';//dmsg[userip.substring(7)][0];
-    dmsg.uname="Anonymous";//dmsg[userip.substring(7)][1];
-    io.emit('dchat msg', dmsg);
-  });
+    	var userip=socket.request.connection.remoteAddress;
+      console.log(userip);
+    	dmsg.message=msg;//dmsg.debug+userip
+    	dmsg.user=userip;
+    	dmsg.ucolor=dmsg[userip][0];//[colors[Math.floor(Math.random() * 14)]];//
+      dmsg.uname=dmsg[userip][1];//"Anonymous";
+      io.emit('dchat msg', dmsg);
+    });
+
+  socket.on('EnterInChat', function(msg){
+    var ip=socket.request.connection.remoteAddress;
+    var userinfo=[colors[Math.floor(Math.random() * 14)]];
+    dmsg[ip]=userinfo;
+    console.log(ip);
+
+    var username=msg;
+    dmsg[ip][1]=username;
+
+    var destination = '/chatroom';
+    io.emit('redirect', destination);
+    });
 });
 
 http.listen(port);
